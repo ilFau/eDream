8847682ce28345fa55718adb796d14a49d87b3b0
wabri <gabriele.puliti@gmail.com>
2016-03-22 14:57:06 +0100

    non so più che fare

--- EdreamMp/src/calendar/Event.java
+++ EdreamMp/src/calendar/Event.java
@@ -1,7 +1,25 @@
 package calendar;
 
-public interface Event {
+public abstract class Event {
 	
+	private String name;
 	
+	private String description;
 
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
