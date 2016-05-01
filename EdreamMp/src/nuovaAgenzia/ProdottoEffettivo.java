a50e09d79b6bcc03db724d355618287d8ac164f6
wabri <gabriele.puliti@gmail.com>
2016-05-01 13:19:45 +0200

    mi sono permesso di eliminare la classe data, dato che java.util.* ha già la sua. ritengo quindi inutile crearne una nuova.

--- EdreamMp/src/nuovaAgenzia/ProdottoEffettivo.java
+++ EdreamMp/src/nuovaAgenzia/ProdottoEffettivo.java
@@ -1,9 +1,11 @@
 package nuovaAgenzia;
 
-import java.util.List;
+import java.util.*;
 
 public class ProdottoEffettivo extends Prodotto{
 
+	Date data;
+	
 	public ProdottoEffettivo(Tratte T, List<Dipendente> LDH, List<Dipendente> LDA, List<Autobus> LA) {
 		super(T, LDH, LDA, LA);
 	}
