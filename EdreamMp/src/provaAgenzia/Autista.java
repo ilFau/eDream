861b33e680832db2c439cfa0841ea2d9259e9ed0
wabri <gabriele.puliti@gmail.com>
2016-05-03 16:12:09 +0200

    aggiornate interfacce

--- EdreamMp/src/provaAgenzia/Autista.java
+++ EdreamMp/src/provaAgenzia/Autista.java
@@ -1,5 +1,14 @@
 package provaAgenzia;
 
+import java.util.*;
+
 public abstract class Autista implements Dipendenti {
+	
+	private List<Prodotto> listaLavori;
+	
+	public Autista(String nome, String cognome, String codiceFiscale) {
+	}
+	
+	
 
 }
