e94304178c4f227e10bd1e6a44ab1f82d4fcd255
wabri <gabriele.puliti@gmail.com>
2016-05-03 16:47:58 +0200

    aggiornata la classe Truppa

--- EdreamMp/src/provaAgenzia/ProdottoEffettivo.java
+++ EdreamMp/src/provaAgenzia/ProdottoEffettivo.java
@@ -9,4 +9,6 @@ public abstract class ProdottoEffettivo {
 	private Date dataArrivo;
 	private Truppa oggetti;
 
+	
+	
 }
--- EdreamMp/src/provaAgenzia/Truppa.java
+++ EdreamMp/src/provaAgenzia/Truppa.java
@@ -4,9 +4,45 @@ import java.util.*;
 
 public abstract class Truppa {
 	
-	private List<Hostess> listaHostess;
+	private List<Hostess> hostess;
 	private List<Autista> autisti;
-	private List<Autobus> autobusDisponibili;
+	private List<Autobus> autobus;
+	
+	public void addHostess (Hostess nuovaHostess) {
+		this.hostess.add(nuovaHostess);
+	}
+	
+	public void addAutisti (Autista nuovoAutista) {
+		this.autisti.add(nuovoAutista);		
+	}
+	
+	public void addAutobus (Autobus nuovoAutobus) {
+		this.autobus.add(nuovoAutobus);
+	}
+	
+	public Iterator<Hostess> getHostess() {
+		return hostess.iterator();
+	}
+	
+	public Iterator<Autista> getAutisti() {
+		return autisti.iterator();
+	}
+	
+	public Iterator<Autobus> getAutobus () {
+		return autobus.iterator();
+	}
+	
+	public int numeroPostiDisponibili() {
+		Iterator<Autobus> iteratorAutobus = getAutobus();
+		Autobus autobusAttuale = iteratorAutobus.next();
+		int totCapienza = autobusAttuale.getCapienza();
+		while (iteratorAutobus.hasNext()) {
+			autobusAttuale = iteratorAutobus.next();
+			totCapienza += autobusAttuale.getCapienza();
+		}
+		return totCapienza;
+	}
+	
 	
 
 }
